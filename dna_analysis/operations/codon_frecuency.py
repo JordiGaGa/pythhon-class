6ca36ada0b7c30d6b495e19fb5cd967eeffd48c2
JordiGaGa <jordigg@lcg.unam.mx>
2024-05-11 09:24:12 -0600

    Codon_frecuency importando función de otro módulo

--- dna_analysis/operations/codon_frecuency.py
+++ dna_analysis/operations/codon_frecuency.py
@@ -1,12 +1,10 @@
+import sys
+sys.path.append("C:/Users/jordi/OneDrive/Escritorio/Programacion_Python/python-class/dna_analysis/utils")
+from validators import validate_dna_sequence
+
 def codon_frecuency(sequence):
-    # Validacion de existencia de secuencia
-    if not sequence:
-        raise ValueError("La secuencia proporcionada esta vacia.")
-    #Validacion de caracteres
-    sequence = sequence.upper()
-    if any(c not in 'ACGTN' for c in sequence):
-        raise ValueError("La secuencia contiene caracteres no validos.")
-    
+
+    validate_dna_sequence(sequence)
     #Diccionario de codones
     dic_codones = {}
     #Se va analizando de 3 en tres la secuencia
@@ -21,3 +19,20 @@ def codon_frecuency(sequence):
 
 sequence = "ATGTGCTAGCGTATG"
 print(codon_frecuency(sequence))
+
+
+
+
+
+
+
+
+"""
+    # Validacion de existencia de secuencia
+    if not sequence:
+        raise ValueError("La secuencia proporcionada esta vacia.")
+    #Validacion de caracteres
+    sequence = sequence.upper()
+    if any(c not in 'ACGTN' for c in sequence):
+        raise ValueError("La secuencia contiene caracteres no validos.")
+"""
